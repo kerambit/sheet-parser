470b056abfaeda82da794dfe57a9acddc2ace39b
kerambit <liukang1488666228@gmail.com>
2025-05-26 12:56:18 +0400

    remove unused code

--- test.js
+++ test.js
@@ -1,10 +1,1 @@
-import { URL } from "url";
-import * as fs from "fs/promises";
-
-const parseUrl = new URL(
-  "https://musescore.com/static/musescore/scoredata/g/d619ce509f78fb1a0b925be70fa09c722d218128/score_0.svg?no-cache=1715689499 у меня должен быть filePath = scorre_0.svg",
-);
-
-console.log(parseUrl.pathname.split("/").at(-1));
-
-fs.mkdir("cock");
+// tests
